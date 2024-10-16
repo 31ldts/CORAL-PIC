930d4264278d5e4f4a5a6e652b5341e2ba552749
31ldts <31ldts@gmail.com>
2024-10-16 16:25:22 +0200

    test: add test for config public methods

--- tests.py
+++ tests.py
@@ -1,202 +1,173 @@
 from analyze_interactions import *
 from colorama import Fore, init
-import os
 
+# Directory for test support files
 test_support_directory = "tests_support_files"
-change_directory("outputs")
 
-def tests_cdu_01():
-    def run_tests(test_cases, expected_output, exceptions, test_name, save=False):
-        print(f"Running {test_name}:")
+def check_test_result(result: bool, expected: bool, test_case_index: int, message: str) -> int:
+    """
+    Check if the test result matches the expected output and print the corresponding message.
+
+    Args:
+        result (bool): The actual result of the test case.
+        expected (bool): The expected result of the test case.
+        test_case_index (int): The index of the test case.
+
+    Returns:
+        int: 1 if the test passed, 0 if it failed.
+    """
+    if result == expected:
+        print(f"{Fore.LIGHTGREEN_EX}\tTest case {test_case_index} passed correctly{message}")
+        return 1
+    else:
+        print(f"{Fore.LIGHTRED_EX}\tTest case {test_case_index} failed. Expected {'pass' if expected else 'fail'}, but got {'pass' if result else 'fail'}.")
+        return 0
+
+def print_final_result(passed_tests: int, total_tests: int) -> None:
+    """
+    Print the final result of the test cases.
+
+    Args:
+        passed_tests (int): The number of tests that passed.
+        total_tests (int): The total number of tests executed.
+    """
+    result_message = f"Tests passed ({passed_tests}/{total_tests}).\n"
+    print(f"{Fore.LIGHTGREEN_EX if passed_tests == total_tests else Fore.LIGHTRED_EX}{result_message}")
+
+def test_change_directory(analyzer: AnalyzeInteractions) -> None:
+    """
+    Test the change_directory method of the AnalyzeInteractions class.
+    
+    Args:
+        analyzer (AnalyzeInteractions): An instance of the AnalyzeInteractions class.
+    """
+    def run_tests(test_cases, expected_output, exceptions, test_name) -> None:
+        """
+        Execute test cases and validate results.
+
+        Args:
+            test_cases (list): List of dictionaries containing test parameters.
+            expected_output (list): Expected results for each test case.
+            exceptions (tuple): Exception types expected during tests.
+            test_name (str): Name of the test being executed.
+        """
+        print(f"Testing {test_name}:")
         passed_tests = 0
 
         for i, (case, expected) in enumerate(zip(test_cases, expected_output), start=1):
-            
+            message = "."
             try:
-                output = analyze_files(**case)
+                analyzer.change_directory(**case)
                 result = True
-                if save:
-                    save_matrix(matrix=output, filename=f'{test_name}-{i}.csv')
             except exceptions as e:
                 result = False
-                print(f"{Fore.LIGHTYELLOW_EX}\tTest case {i} failed with error: {e}")            
+                message = ": " + e.args[0] + "."
 
-            # Check if the test result matches the expected output
-            if result == expected:
-                print(f"{Fore.LIGHTGREEN_EX}\tTest case {i} passed correctly.")
-                passed_tests += 1
-            else:
-                print(f"{Fore.LIGHTRED_EX}\tTest case {i} failed. Expected {'pass' if expected else 'fail'}, but got {'pass' if result else 'fail'}.")
+            passed_tests += check_test_result(result=result, expected=expected, test_case_index=i, message=message)
+        
+        print_final_result(passed_tests=passed_tests, total_tests=len(test_cases))
 
-        # Print the final result
-        result_message = f"Tests passed ({passed_tests}/{len(test_cases)}).\n"
-        if passed_tests == len(test_cases):
-            print(f"{Fore.LIGHTGREEN_EX}{result_message}")
-        else:
-            print(f"{Fore.LIGHTRED_EX}{result_message}")
+    # Test cases for change_directory
+    cases = [
+        {"path": "outputs"},
+        {"path": "wrong_dir"},
+        {"path": 1}
+    ]
+    expected = [True, False, False]
+    exceptions = (ValueError, TypeMismatchException)
+
+    run_tests(cases, expected, exceptions, "change_directory")
+
+def test_set_plot_config(analyzer: AnalyzeInteractions) -> None:
+    """
+    Test the set_plot_config method of the AnalyzeInteractions class.
     
-    # Cdu 01.T1
-    test_1_cases = [
-        {"directory": "ichem-ifp", "activity_file": "ichem-ifp-activities.csv", "protein": True, "ligand": True, "subunit": True},
-        {"directory": 1, "activity_file": "ichem-ifp-activities.csv", "protein": True, "ligand": True, "subunit": True},
-        {"directory": "ichem-ifp", "activity_file": True, "protein": True, "ligand": True, "subunit": True},
-        {"directory": "ichem-ifp", "activity_file": "ichem-ifp-activities.csv", "protein": True, "ligand": "True", "subunit": True},
-        {"directory": "ichem-ifp", "activity_file": "ichem-ifp-activities.csv", "ligand": True, "subunit": True},
-        {"directory": "ichem-ifp", "protein": True, "ligand": True, "subunit": True}
-    ]
-    expected_1 = [True, False, False, False, True, True]
-    exceptions_1 = TypeMismatchException
+    Args:
+        analyzer (AnalyzeInteractions): An instance of the AnalyzeInteractions class.
+    """
+    def run_tests(test_cases, expected_output, exceptions, test_name) -> None:
+        """
+        Execute test cases for set_plot_config and validate results.
 
-    run_tests(test_1_cases, expected_1, exceptions_1, "Cdu 01.T1")
-
-    # Cdu 01.T2
-    test_2_cases = [
-        {"directory": "ichem-ifp", "activity_file": "ichem-ifp-activities.csv", "protein": True, "ligand": True, "subunit": True},
-        {"directory": "ichem", "activity_file": "ichem-ifp-activities.csv", "protein": True, "ligand": True, "subunit": True},
-        {"directory": os.path.join(test_support_directory, "cdu 01. T2: Case 3"), "activity_file": "ichem-ifp-activities.csv", "protein": True, "ligand": True, "subunit": True},
-        {"directory": os.path.join(test_support_directory, "cdu 01. T2: Case 4"), "activity_file": "ichem-ifp-activities.csv", "protein": True, "ligand": True, "subunit": True},
-        {"directory": os.path.join(test_support_directory, "cdu 01. T2: Case 5"), "activity_file": "ichem-ifp-activities.csv", "protein": True, "ligand": True, "subunit": True},
-        {"activity_file": "ichem-ifp-activities.csv", "protein": True, "ligand": True, "subunit": True}
-    ]
-    expected_2 = [True, False, False, True, True, False]
-    exceptions_2 = (FileOrDirectoryNotFoundException, EmptyDirectoryException, TypeError)
-
-    run_tests(test_2_cases, expected_2, exceptions_2, "Cdu 01.T2")
-
-    # Cdu 01.T3
-    test_3_cases = [
-        {"directory": "ichem-ifp", "activity_file": "ichem-ifp-activities.csv", "protein": True, "ligand": True, "subunit": True},
-        {"directory": "ichem-ifp", "activity_file": "activities.csv", "protein": True, "ligand": True, "subunit": True},
-        {"directory": "ichem-ifp", "protein": True, "ligand": True, "subunit": True},
-        {"directory": "ichem-ifp", "activity_file": os.path.join(test_support_directory, "cdu 01. T3: Case 4.csv"), "protein": True, "ligand": True, "subunit": True},
-        {"directory": "ichem-ifp", "activity_file": os.path.join(test_support_directory, "cdu 01. T3: Case 5.csv"), "protein": True, "ligand": True, "subunit": True},
-        {"directory": "ichem-ifp", "activity_file": os.path.join(test_support_directory, "cdu 01. T3: Case 6.csv"), "protein": True, "ligand": True, "subunit": True},
-        {"directory": "ichem-ifp", "activity_file": os.path.join(test_support_directory, "cdu 01. T3: Case 7.csv"), "protein": True, "ligand": True, "subunit": True}
-    ]
-    expected_3 = [True, False, True, False, False, True, True]
-    exceptions_3 = (FileOrDirectoryNotFoundException, EmptyDirectoryException, TypeError, FileNotFoundError, ValueError)
-
-    run_tests(test_3_cases, expected_3, exceptions_3, "Cdu 01.T3", True)
-
-    # Cdu 01.T4
-    test_4_cases = [
-        {"directory": "ichem-ifp", "activity_file": "ichem-ifp-activities.csv", "protein": False, "ligand": False, "subunit": False},
-        {"directory": "ichem-ifp", "activity_file": "ichem-ifp-activities.csv", "protein": True, "ligand": False, "subunit": False},
-        {"directory": "ichem-ifp", "activity_file": "ichem-ifp-activities.csv", "protein": True, "ligand": True, "subunit": False},
-        {"directory": "ichem-ifp", "activity_file": "ichem-ifp-activities.csv", "protein": True, "ligand": False, "subunit": True},
-        {"directory": "ichem-ifp", "activity_file": "ichem-ifp-activities.csv", "protein": False, "ligand": True, "subunit": False},
-        {"directory": "ichem-ifp", "activity_file": "ichem-ifp-activities.csv", "protein": False, "ligand": True, "subunit": True},
-        {"directory": "ichem-ifp", "activity_file": "ichem-ifp-activities.csv", "protein": False, "ligand": False, "subunit": True},
-        {"directory": "ichem-ifp", "activity_file": "ichem-ifp-activities.csv", "protein": True, "ligand": True, "subunit": True},
-    ]
-    expected_4 = [True, True, True, True, True, True, True, True]
-    exceptions_4 = (FileOrDirectoryNotFoundException, EmptyDirectoryException, TypeError, FileNotFoundError, ValueError)
-
-    run_tests(test_4_cases, expected_4, exceptions_4, "Cdu 01.T4", True)
-
-def tests_cdu_02():
-    def run_tests(test_cases, expected_output, exceptions, test_name, save=False):
-        print(f"Running {test_name}:")
+        Args:
+            test_cases (list): List of dictionaries containing test parameters.
+            expected_output (list): Expected results for each test case.
+            exceptions (tuple): Exception types expected during tests.
+            test_name (str): Name of the test being executed.
+        """
+        print(f"Testing {test_name}:")
         passed_tests = 0
 
         for i, (case, expected) in enumerate(zip(test_cases, expected_output), start=1):
-            
+            message = "."
             try:
-                output = sort_matrix(**case)
-                result = True
-                if save:
-                    save_matrix(matrix=output, filename=f'{test_name}-{i}.csv')
+                analyzer.set_plot_config(**case)
+
+                # Initialize result as False
+                result = False  
+
+                # Check conditions to set result to True
+                if 'reset' in case:
+                    result = (case['reset'] and analyzer.interaction_labels == INTERACTION_LABELS and analyzer.colors == COLORS) or not case['reset']
+                elif 'colors' in case and 'interactions' in case:
+                    result = (case['colors'] == analyzer.colors and case['interactions'] == analyzer.interaction_labels)
+                elif 'colors' in case:
+                    result = case['colors'] == analyzer.colors
+                elif 'interactions' in case:
+                    result = case['interactions'] == analyzer.interaction_labels
+
             except exceptions as e:
                 result = False
-                print(f"{Fore.LIGHTYELLOW_EX}\tTest case {i} failed with error: {e}")            
+                message = f": {e.args[0]}"
 
-            # Check if the test result matches the expected output
-            if result == expected:
-                print(f"{Fore.LIGHTGREEN_EX}\tTest case {i} passed correctly.")
-                passed_tests += 1
-            else:
-                print(f"{Fore.LIGHTRED_EX}\tTest case {i} failed. Expected {'pass' if expected else 'fail'}, but got {'pass' if result else 'fail'}.")
+            passed_tests += check_test_result(result=result, expected=expected, test_case_index=i, message=message)
+        
+        print_final_result(passed_tests=passed_tests, total_tests=len(test_cases))
 
-        # Print the final result
-        result_message = f"Tests passed ({passed_tests}/{len(test_cases)}).\n"
-        if passed_tests == len(test_cases):
-            print(f"{Fore.LIGHTGREEN_EX}{result_message}")
-        else:
-            print(f"{Fore.LIGHTRED_EX}{result_message}")
-    
-    matrix = analyze_files(directory="ichem-ifp", activity_file="ichem-ifp-activities.csv", protein=True, ligand=True, subunit=False)
+    # Test data
+    interactions = ["Hydrophobic", "Aromatic_Face/Face"]
+    colors_valids = ["#ff6384", "#36a2eb"]
+    colors_no_valids = ["ff6384", "#36x2eb"]
+    interactions_bigger = ["Hydrophobic", "Aromatic_Face/Face", "Other"]
+    colors_bigger = ["#ff6384", "#36a2eb", "#36a45b"]
 
-    # Cdu 02.T1
-    test_1_cases = [
-        {"matrix": matrix, "axis": "columns", "thr_interactions": 7, "count": True, "residue_chain": True},
-        {"matrix": matrix, "axis": "columns", "thr_activity": 6.1, "count": True, "residue_chain": True},
-        {"matrix": matrix, "axis": "columns", "selected_items": 4, "count": True, "residue_chain": True},
-        {"matrix": 'matrix', "axis": "columns", "thr_interactions": 7, "thr_activity": 6.1, "selected_items": 4, "count": True, "residue_chain": True},
-        {"matrix": matrix, "axis": 23, "thr_interactions": 7, "thr_activity": 6.1, "selected_items": 4, "count": True, "residue_chain": True},
-        {"matrix": matrix, "axis": "fila", "selected_items": 4, "count": True, "residue_chain": True},
-        {"matrix": matrix, "axis": "columns", "thr_interactions": 2.3, "count": True, "residue_chain": True},
-        {"matrix": matrix, "axis": "columns", "thr_interactions": 7, "thr_activity": 6, "selected_items": 4, "count": True, "residue_chain": True},
-        {"matrix": matrix, "axis": "columns", "thr_interactions": 7, "thr_activity": True, "selected_items": 4, "count": True, "residue_chain": True},
-        {"matrix": matrix, "axis": "columns", "thr_interactions": 7, "thr_activity": 6.1, "selected_items": 4.8, "count": True, "residue_chain": True},
-        {"matrix": matrix, "axis": "columns", "thr_interactions": 7, "thr_activity": 6.1, "selected_items": 'False', "count": True, "residue_chain": True},
-        {"matrix": matrix, "axis": "columns", "thr_interactions": 7, "thr_activity": 6.1, "selected_items": 4, "count": 4, "residue_chain": True},
-        {"matrix": matrix, "axis": "columns", "thr_interactions": 7, "thr_activity": 6.1, "selected_items": 4, "count": True, "residue_chain": 'True'},
-        {"matrix": matrix, "thr_interactions": 7, "count": True, "residue_chain": True},
-        {"matrix": matrix, "axis": "columns", "count": True, "residue_chain": True},
-        {"matrix": matrix, "axis": "columns", "thr_interactions": 7, "residue_chain": True},
-        {"matrix": matrix, "axis": "columns", "thr_interactions": 7, "count": True}
+    # Test cases for set_plot_config
+    cases = [
+        {"interactions": interactions, "colors": colors_valids, "reset": False},  # Valid configuration
+        {"interactions": 123, "colors": colors_valids, "reset": False},  # Invalid types
+        {"interactions": interactions, "colors": True, "reset": False},
+        {"interactions": interactions, "colors": colors_valids, "reset": "False"},
+        {"interactions": interactions, "colors": colors_no_valids, "reset": False},
+        {"colors": colors_valids, "reset": False},  # Partial updates
+        {"interactions": interactions, "reset": False},
+        {"interactions": interactions, "colors": colors_valids},
+        {"reset": True},  # Reset cases
+        {"reset": False},
+        {},  # Empty input
+        {"interactions": interactions_bigger, "colors": colors_valids},  # Size mismatches
+        {"interactions": interactions, "colors": colors_bigger}
     ]
-    expected_1 = [True, True, True, False, False, True, False, False, False, False, False, False, False, True, True, True, True]
-    exceptions_1 = TypeMismatchException
-    
-    run_tests(test_1_cases, expected_1, exceptions_1, "Cdu 02.T1")
 
-def tests_cdu_03():
-    def run_tests(test_cases, expected_output, exceptions, test_name, save=False):
-        print(f"Running {test_name}:")
-        passed_tests = 0
-
-        for i, (case, expected) in enumerate(zip(test_cases, expected_output), start=1):
-            
-            try:
-                output = transpose_matrix(**case)
-                result = True
-                if save:
-                    save_matrix(matrix=output, filename=f'{test_name}-{i}.csv')
-            except exceptions as e:
-                result = False
-                print(f"{Fore.LIGHTYELLOW_EX}\tTest case {i} failed with error: {e}")            
-
-            # Check if the test result matches the expected output
-            if result == expected:
-                print(f"{Fore.LIGHTGREEN_EX}\tTest case {i} passed correctly.")
-                passed_tests += 1
-            else:
-                print(f"{Fore.LIGHTRED_EX}\tTest case {i} failed. Expected {'pass' if expected else 'fail'}, but got {'pass' if result else 'fail'}.")
-
-        # Print the final result
-        result_message = f"Tests passed ({passed_tests}/{len(test_cases)}).\n"
-        if passed_tests == len(test_cases):
-            print(f"{Fore.LIGHTGREEN_EX}{result_message}")
-        else:
-            print(f"{Fore.LIGHTRED_EX}{result_message}")
-    
-    matrix = analyze_files(directory="ichem-ifp", activity_file="ichem-ifp-activities.csv", protein=True, ligand=True, subunit=False)
-
-    # Cdu 03.T1
-    test_1_cases = [
-        {"matrix": matrix},
-        {"matrix": 12}
+    expected = [
+        True,  # Valid configuration
+        False, False, False, False,  # Invalid types or invalid colors
+        True, True, True,  # Partial updates
+        True, True,  # Reset cases
+        False,  # Empty input (no valid config)
+        True, True  # Size mismatches allowed
     ]
-    expected_1 = [True, False, False, False, True, True]
-    exceptions_1 = TypeMismatchException
 
-    run_tests(test_1_cases, expected_1, exceptions_1, "Cdu 03.T1")
+    exceptions = (ValueError, TypeMismatchException, InvalidColorException)
+    
+    # Execute the tests
+    run_tests(cases, expected, exceptions, "set_plot_config")
 
-# Initialize colorama
+# Initialize colorama for colored output
 init(autoreset=True)
 
-# Run all tests
-#tests_cdu_01()
-#tests_cdu_02()
-tests_cdu_03()
+# Create an instance of AnalyzeInteractions
+analyzer = AnalyzeInteractions()
+
+# Execute all test functions
+test_change_directory(analyzer=analyzer)
+test_set_plot_config(analyzer=analyzer)
