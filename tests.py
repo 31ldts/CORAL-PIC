f94c7d9906be891b3460d0819c49ce5e9d4378ee
31ldts <31ldts@gmail.com>
2024-10-18 11:54:24 +0200

    feat: add globals to show global tests performance test: add test for remove_empty_axis

--- tests.py
+++ tests.py
@@ -5,6 +5,8 @@ import sys
 
 # Directory for test support files
 test_support_directory = "tests_support_files"
+total_tests = 0
+total_passed_tests = 0
 
 def load_and_compare_csv(directory: str, filename: str, result: list[list[str]]) -> bool:
     """
@@ -91,7 +93,7 @@ def print_final_result(passed_tests: int, total_tests: int) -> None:
         passed_tests (int): The number of tests that passed.
         total_tests (int): The total number of tests executed.
     """
-    result_message = f"Tests passed ({passed_tests}/{total_tests}).\n"
+    result_message = f"Passed tests ({passed_tests}/{total_tests}).\n"
     print(f"{Fore.LIGHTGREEN_EX if passed_tests == total_tests else Fore.LIGHTRED_EX}{result_message}")
 
 def run_tests(
@@ -142,6 +144,9 @@ def run_tests(
 
     # Print the final summary of tests
     print_final_result(passed_tests=passed_tests, total_tests=len(test_cases))
+    global total_tests, total_passed_tests
+    total_tests += len(test_cases)
+    total_passed_tests += passed_tests
 
 def validate_change_directory_result(analyzer: AnalyzeInteractions, case: dict) -> bool:
     """Validation logic for change_directory."""
@@ -297,7 +302,6 @@ def test_transpose_matrix(analyzer: AnalyzeInteractions) -> None:
         validate_result=validate_transpose_matrix_result
     )
 
-
 def validate_save_matrix_result(analyzer: AnalyzeInteractions, case: dict) -> bool:
     """Validation logic for transpose_matrix."""
 
@@ -347,6 +351,81 @@ def test_save_matrix(analyzer: AnalyzeInteractions) -> None:
     )
 
 
+def validate_remove_empty_axis_result(analyzer: AnalyzeInteractions, case: dict) -> bool:
+    """Validation logic for transpose_matrix."""
+
+    empty_chars = [EMPTY_CELL, EMPTY_DASH_CELL]
+
+    def is_empty_row(row: list[str]) -> bool:
+        return all(cell in empty_chars for cell in row[1:])
+
+    def is_empty_column(column: tuple[str]) -> bool:
+        return all(cell in empty_chars for cell in column[1:])
+
+    matrix = case['matrix']
+    
+    # Remove empty rows
+    matrix = [row for row in matrix if not is_empty_row(row)]
+
+    # Transpose the filtered matrix to easily remove empty columns
+    matrix = list(zip(*matrix))
+
+    # Remove empty columns
+    matrix = [column for column in matrix if not is_empty_column(column)]
+
+    # Transpose back to the original orientation and convert tuples to lists
+    matrix = [list(row) for row in zip(*matrix)]
+
+    # Compare the final_matrix with the expected result
+    return matrix == case['result']
+
+def test_remove_empty_axis(analyzer: AnalyzeInteractions) -> None:
+    """
+    Test the remove_empty_axis method of the AnalyzeInteractions class.
+    
+    Args:
+        analyzer (AnalyzeInteractions): An instance of the AnalyzeInteractions class.
+
+    Returns:
+        None
+    """
+
+    matrix_1 = [['abc', '123', 'hij', '456'], 
+                ['klm', EMPTY_DASH_CELL, 'qrs', EMPTY_CELL],
+                ['klm', EMPTY_DASH_CELL, 'qrs', EMPTY_CELL]]
+    matrix_2 = [['123','456','789'], 
+                ['abc', 'def', 'hij'], 
+                ['123', EMPTY_CELL, EMPTY_DASH_CELL], 
+                ['klm', 'nop', 'qrs']]
+    cases = [
+        {"matrix": matrix_1, "save": 'test.csv'},  # Valid configuration
+        {"matrix": matrix_1, "save": False},  # Invalid types
+        {"matrix": False, "save": 'test.csv'},
+        {"matrix": matrix_2}, # Partial updates
+        {"save": 'test.csv'},
+        {},  # Empty input
+    ]
+
+    expected = [
+        True,  # Valid configuration
+        False, False, # Invalid types or invalid colors
+        True, False, # Partial updates
+        False,  # Empty input (no valid config)
+        False   # Invalid file extension
+    ]
+
+    exceptions = (ValueError, TypeMismatchException, TypeError, InvalidFileExtensionException)
+
+    run_tests(
+        analyzer=analyzer,
+        method_name="remove_empty_axis",
+        test_cases=cases,
+        expected_output=expected,
+        exceptions=exceptions,
+        validate_result=validate_remove_empty_axis_result
+    )
+
+
 # Initialize colorama
 init(autoreset=True)
 create_directory(test_support_directory)
@@ -357,3 +436,7 @@ test_change_directory(analyzer)
 test_set_plot_config(analyzer)
 test_transpose_matrix(analyzer)
 test_save_matrix(analyzer)
+test_remove_empty_axis(analyzer)
+
+result_message = f"Total passed tests ({total_passed_tests}/{total_tests}).\n"
+print(f"{Fore.LIGHTGREEN_EX if total_passed_tests == total_tests else Fore.LIGHTRED_EX}{result_message}")
